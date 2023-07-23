25cbea4ede7b47e2b63a07bb46cb691db107a8e1
Dillon Reedy <dillon.developing@outlook.com>
2023-07-23 15:38:05 -0500

    Added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Particles } from 'react-tsparticles';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles
+        id="tsparticles"
+        options={{
+          background: {
+            color: {
+              value: '#0d47a1',
+            },
+          },
+          fpsLimit: 60,
+          particles: {
+            number: {
+              value: 80,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+            color: {
+              value: '#ffffff',
+            },
+            shape: {
+              type: 'circle',
+            },
+            opacity: {
+              value: 0.5,
+            },
+            size: {
+              value: 5,
+            },
+            move: {
+              direction: 'none',
+              speed: 2,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
     </div>
   );
 }
